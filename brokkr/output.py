9edf921644078c6f19d1ecd5a0c353864e016fb7
C.A.M. Gerlach <CAM.Gerlach@Gerlach.CAM>
2019-07-27 01:54:50 -0500

    Add code to output recorded data to CSV file

---
+++ brokkr/output.py
@@ -0,0 +1,28 @@
+"""
+Routines to write out collected status data to a CSV file.
+"""
+
+import csv
+import io
+
+
+def write_line_csv(data, file):
+    try:
+        if isinstance(file, io.IOBase):
+            close_file = False
+            data_csv = file
+        else:
+            close_file = True
+            data_csv = open(file, mode="a",
+                            encoding="utf-8", newline="")
+        csv_writer = csv.DictWriter(data_csv, fieldnames=data.keys(),
+                                    dialect="unix", delimiter=",")
+        if not data_csv.tell():
+            csv_writer.writeheader()
+        csv_writer.writerow(data)
+    finally:
+        if close_file:
+            try:
+                data_csv.close()
+            except Exception:
+                pass
