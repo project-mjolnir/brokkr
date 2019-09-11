7d59f7d4e431fdaf6038eef22c3636699f9cb840
C.A.M. Gerlach <CAM.Gerlach@Gerlach.CAM>
2019-09-10 20:04:57 -0500

    Add logging and additional handling to ping function/sensor module

--- brokkr/sensor.py
+++ brokkr/sensor.py
@@ -3,6 +3,7 @@ Routines to get status information from a HAMMA2 sensor over Ethernet.
 """
 
 # Standard library imports
+import logging
 import platform
 import subprocess
 
@@ -10,16 +11,46 @@ import subprocess
 from config import CONFIG
 
 
+logger = logging.getLogger(__name__)
+
+
 def ping(host=CONFIG["general"]["sensor_ip"],
          timeout=CONFIG["monitor"]["sensor"]["ping_timeout_s"]):
     # Set the correct option for the number of packets based on platform.
     if platform.system().lower() == "windows":
-        param = "-n"
+        count_param = "-n"
     else:
-        param = "-c"
+        count_param = "-c"
 
-    # Build the command, e.g. ``ping -c 1 google.com``
-    command = ["ping", param, "1", "-w", str(timeout), host]
+    # Build the command, e.g. ping -c 1 -w 1 10.10.10.1
+    command = ["ping", count_param, "1", "-w", str(timeout), host]
+    logger.debug("Running ping command: %s", " ".join(command))
+    if logger.getEffectiveLevel() <= logging.DEBUG:
+        extra_args = {
+                      "stdout": subprocess.PIPE,
+                      "stderr": subprocess.PIPE,
+                      "encoding": "utf-8",
+                      "errors": "surrogateescape",
+                      }
+    else:
+        extra_args = {
+                      "stdout": subprocess.DEVNULL,
+                      "stderr": subprocess.DEVNULL,
+                      }
 
-    return subprocess.run(command, stdout=subprocess.DEVNULL,
-                          stderr=subprocess.DEVNULL).returncode
+    try:
+        ping_output = subprocess.run(command, timeout=timeout + 1,
+                                     **extra_args)
+    except subprocess.TimeoutExpired:
+        logger.info("Timeout expired running ping command %s",
+                    " ".join(command))
+        logger.debug("Details:", exc_info=1)
+        return -1
+    except Exception as e:
+        logger.warning("%s running ping command %s: %s",
+                       type(e), " ".join(command), e)
+        logger.info("Details:", exc_info=1)
+        return -9
+
+    logger.debug("Ping command output: %s", ping_output)
+    return ping_output.returncode
