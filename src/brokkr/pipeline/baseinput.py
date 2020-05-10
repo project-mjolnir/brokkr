29b4fc7f2a62364cad593f8b5afdc323886d671f
C.A.M. Gerlach <CAM.Gerlach@Gerlach.CAM>
2020-05-09 22:01:46 -0500

    Add support for arbitrary I2C and SMBus devices with smbus2

--- src/brokkr/pipeline/baseinput.py
+++ src/brokkr/pipeline/baseinput.py
@@ -22,6 +22,7 @@ class ValueInputStep(brokkr.pipeline.base.InputStep, metaclass=abc.ABCMeta):
             datatype_default_kwargs=None,
             conversion_functions=None,
             na_marker=None,
+            include_all_data_each=False,
             **pipeline_step_kwargs):
         super().__init__(**pipeline_step_kwargs)
         if datatype_default_kwargs is None:
@@ -52,6 +53,7 @@ class ValueInputStep(brokkr.pipeline.base.InputStep, metaclass=abc.ABCMeta):
             data_types=self.data_types,
             conversion_functions=conversion_functions,
             na_marker=na_marker,
+            include_all_data_each=include_all_data_each,
             )
 
     @abc.abstractmethod
@@ -59,7 +61,7 @@ class ValueInputStep(brokkr.pipeline.base.InputStep, metaclass=abc.ABCMeta):
         pass
 
     def decode_data(self, raw_data):
-        self.logger.debug("Created data decoder: %r", self.decoder)
+        # self.logger.debug("Created data decoder: %r", self.decoder)
         decoded_data = self.decoder.decode_data(raw_data)
         return decoded_data
 
@@ -73,20 +75,23 @@ class ValueInputStep(brokkr.pipeline.base.InputStep, metaclass=abc.ABCMeta):
 class SensorInputStep(ValueInputStep, metaclass=abc.ABCMeta):
     def __init__(
             self,
-            sensor_module,
             sensor_class,
+            sensor_module=None,
             sensor_args=None,
             sensor_kwargs=None,
             cache_sensor_object=False,
+            binary_decoder=False,
             **value_input_kwargs):
-        super().__init__(binary_decoder=False, **value_input_kwargs)
+        super().__init__(binary_decoder=binary_decoder, **value_input_kwargs)
         self.sensor_args = () if sensor_args is None else sensor_args
         self.sensor_kwargs = {} if sensor_kwargs is None else sensor_kwargs
         self.sensor_object = None
         self.cache_sensor_object = cache_sensor_object
 
-        module_object = importlib.import_module(sensor_module)
-        self.object_class = getattr(module_object, sensor_class)
+        if sensor_module:
+            module_object = importlib.import_module(sensor_module)
+            sensor_class = getattr(module_object, sensor_class)
+        self.object_class = sensor_class
 
     def init_sensor_object(self, *sensor_args, **sensor_kwargs):
         if not sensor_args:
@@ -111,18 +116,33 @@ class SensorInputStep(ValueInputStep, metaclass=abc.ABCMeta):
             self.sensor_object = sensor_object
         return sensor_object
 
+    def get_sensor_object(self, sensor_object=None):
+        if sensor_object is not None:
+            return sensor_object
+        if self.sensor_object is not None:
+            return self.sensor_object
+
+        sensor_object = self.init_sensor_object()
+        return sensor_object
+
+    @abc.abstractmethod
+    def read_sensor_data(self, sensor_object=None):
+        pass
+
+    def read_raw_data(self, input_data=None):
+        raw_data = self.read_sensor_data()
+        return raw_data
+
+
+class AttributeInputStep(SensorInputStep, metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def read_sensor_value(self, sensor_object, data_type):
         pass
 
     def read_sensor_data(self, sensor_object=None):
+        sensor_object = self.get_sensor_object(sensor_object=sensor_object)
         if sensor_object is None:
-            sensor_object = self.sensor_object
-
-        if sensor_object is None:
-            sensor_object = self.init_sensor_object()
-            if sensor_object is None:
-                return None
+            return None
 
         sensor_data = []
         for data_type in self.data_types:
@@ -131,33 +151,29 @@ class SensorInputStep(ValueInputStep, metaclass=abc.ABCMeta):
                     sensor_object=sensor_object, data_type=data_type)
             except Exception as e:
                 self.logger.error(
-                    "%s getting attribute %s from %s sensor object %s "
+                    "%s on attribute %s from %s sensor object %s "
                     "on step %s: %s",
-                    type(e).__name__, data_type.property_name, type(self),
+                    type(e).__name__, data_type.attribute_name, type(self),
                     type(self.object_class), self.name, e)
                 self.logger.info("Error details:", exc_info=True)
                 data_value = None
             sensor_data.append(data_value)
         return sensor_data
 
-    def read_raw_data(self, input_data=None):
-        raw_data = self.read_sensor_data()
-        return raw_data
 
-
-class PropertyInputStep(SensorInputStep):
+class PropertyInputStep(AttributeInputStep):
     def read_sensor_value(self, sensor_object, data_type):
         if sensor_object is None:
             sensor_object = self.sensor_object
-        data_value = getattr(sensor_object, data_type.property_name)
+        data_value = getattr(sensor_object, data_type.attribute_name)
         return data_value
 
 
-class MethodInputStep(SensorInputStep):
+class MethodInputStep(AttributeInputStep):
     def read_sensor_value(self, sensor_object, data_type):
         if sensor_object is None:
             sensor_object = self.sensor_object
         function_kwargs = getattr(data_type, "function_kwargs", {})
-        data_value = getattr(sensor_object, data_type.function_name)(
+        data_value = getattr(sensor_object, data_type.attribute_name)(
             **function_kwargs)
         return data_value
