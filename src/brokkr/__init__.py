775701c5b876a1b831094b4522d23b98da9abb93
C.A.M. Gerlach <CAM.Gerlach@Gerlach.CAM>
2021-03-10 14:17:26 -0600

    Modernize packaging infrastructure to use PEP 517 and setup.cfg

--- src/brokkr/__init__.py
+++ src/brokkr/__init__.py
@@ -2,4 +2,5 @@
 Brokkr, a scientific IoT sensor client for data logging, uplink and control.
 """
 
-from brokkr._version import __version__
+VERSION_INFO = (0, 4, 0, "dev2")
+__version__ = ".".join((str(version) for version in VERSION_INFO))
--- src/brokkr/_version.py
+++
@@ -1,4 +0,0 @@
-"""Version file."""
-
-VERSION_INFO = (0, 4, 0, "dev2")
-__version__ = ".".join((str(version) for version in VERSION_INFO))
